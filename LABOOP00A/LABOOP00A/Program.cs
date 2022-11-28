b787e937c6dc6e642689cec6609a4bb1eaaaa9e6
onpreeyaJa <onpreeya.ja@kkumail.com>
2022-11-28 15:39:08 +0700

    update username

--- LABOOP00A/LABOOP00A/Program.cs
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // print str to commaline
 Console.WriteLine("Onpreeya Jantakote");
+// id
+Console.WriteLine("653450107-5");
+// email
+Console.WriteLine("onpreeya.ja@kkumail.com");
